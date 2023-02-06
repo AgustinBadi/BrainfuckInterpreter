7a6a89ab88efd9c0538ee3aa9cee3653385a997c
AgustinBadi <absalinas@uc.cl>
2023-02-06 12:26:02 -0300

    Add :hammer:: Input operator implemented

--- brainfuck.hs
+++ brainfuck.hs
@@ -40,14 +40,16 @@ parser (x:xs)
 
 
 
---Interpreter
+-- Interpreter
 
 -- Increment cell at pointer
+-- For each element of datatape, if the index element equals to pointer then increment else the element.
 incCell :: DataTape -> Int -> DataTape
 incCell datatape pointer = let len = (length datatape) - 1 
  in [ if pointer == n then succ (datatape !! n) else (datatape !! n) | n <- [0..len] ] 
 
 -- Decrement cell at pointer
+-- For each element of datatape, if the index element equals to pointer then decrement else the element.
 decCell :: DataTape -> Int -> DataTape
 decCell datatape pointer = let len = (length datatape) - 1 
  in [ if pointer == n then pred (datatape !! n) else (datatape !! n) | n <- [0..len] ]
@@ -60,17 +62,25 @@ insertCell datatape element pointer =
 eval :: DataTape -> [BFop] -> Pointer -> IO DataTape
 eval datatape [] _ = return datatape
 eval datatape (op:ops) pointer = case op of
-    Output -> do 
-        print $ chr $ (head . drop pointer) datatape
-        result <- eval datatape ops pointer  
-        return result 
+
     MoveRight -> eval datatape ops (succ pointer)
     MoveLeft -> eval datatape ops (pred pointer)
     Increment -> eval (incCell datatape pointer) ops pointer
     Decrement -> eval (decCell datatape pointer) ops pointer
+
+    Input -> do
+        e <- getChar
+        let insert = insertCell datatape (ord e) pointer
+        eval insert ops pointer
+
+    Output -> do 
+        putChar $ chr $ (head . drop pointer) datatape
+        eval datatape ops pointer  
+
     Loop array -> do 
         loop <- (looping datatape array pointer)
         eval loop ops pointer
+
     where looping :: DataTape -> [BFop] -> Pointer -> IO DataTape
           looping dt ops' p = if dt !! p > 0 
                               then do 
@@ -80,10 +90,16 @@ eval datatape (op:ops) pointer = case op of
                                 return dt
 
 main = do
+    putStr "Set instructions: "
     instructions <- getLine
+    putStr "Set input: "
     let parse = parser instructions
-        bf = eval [0,0,0,0,0,0,0,0] parse 0
-    return bf
+        tape = take 100 $ repeat 0 
+    result <- eval tape parse 0
+    putChar '\n'
+    print result
+
+
 
 
 
